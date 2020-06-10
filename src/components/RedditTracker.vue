d91f196fcb8bc6e0a13a05e87df7db3cfbc2b65c
nicholaspung <nicholaspungdev@gmail.com>
2020-06-09 20:48:18 -0700

    Started working on getting reddit posts, seems to have been restricted to client apps now

---
+++ src/components/RedditTracker.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="reddit-tracker">
+    <h2>Reddit Tracker</h2>
+    <div class="titles-list">
+      <div>
+        <h3>Saved Titles</h3>
+      </div>
+      <div>
+        <h3>Reddit Posts you've missed</h3>
+      </div>
+      <div>
+        <h3>Latest Reddit Posts</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      savedTitles: [],
+      savedRedditPosts: [],
+      redditPosts: [],
+    };
+  },
+  mounted() {
+    axios.get('https://www.reddit.com/r/redditdev/top.json').then((response) => {
+      console.log(response);
+      //   this.redditPosts = reponse.data.children;
+    });
+    // console.log(this.redditPosts);
+  },
+};
+</script>
+
+<style scoped>
+.reddit-tracker h2 {
+  background-color: #c4c4c4;
+  padding: 1rem;
+  margin: 0;
+}
+
+.titles-list {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
--- src/views/App.vue
+++ src/views/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="app">
-    <h2>Reddit Tracker</h2>
+  <div class="container">
+    <reddit-tracker />
   </div>
 </template>
 
+<script>
+import RedditTracker from '@/components/RedditTracker.vue';
+
+export default {
+  components: { RedditTracker },
+};
+</script>
+
+<style>
+#app {
+  justify-content: flex-start;
+  text-align: left;
+}
+</style>
+
 <style scoped>
+.container {
+  margin: 1rem;
+}
 </style>
